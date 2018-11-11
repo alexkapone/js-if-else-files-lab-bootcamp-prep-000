03a56620ca4c12d8990fea87e9c5f9d6cf7ee3fe
Alexander Baron <thebaronalexander@gmail.com>
2018-11-11 06:48:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
-var name = "May May"
-var color = "pink"
-var dick = "Alex"
-var result = "Fucked"
-var safeword = "It hurts"
-var intensity = "too hard"
-
-
-if (dick === "Daddy") {
-  console.log("May May")
-} else {
-  console.log("Dryness")
-}
-
-if (result === "Fucked"){
-  console.log("gets fucked")
-} else {
-  console.log("in the pussy")
-}
-
-if (intensity === "too hard"){
-  console.log("until she says")
-  console.log(safeword)
-}
-
+var name = "Joe"
+var height = "74"
+ 
 // Don't worry about this
-module.exports = { name, color, dick, result, safeword, intensity
+module.exports = { name, height
 }
